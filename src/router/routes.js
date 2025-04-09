30a6d7de333731f4f2fc1b6c9dea61a388c6e31f
pedrotrasfereti <pedrotrasfereti@gmail.com>
2025-04-09 17:04:46 -0300

    feat(router): update routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,16 +3,19 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: '', component: () => import('pages/HomePage.vue') },
+      { path: '/favoritos', component: () => import('pages/FavoritesPage.vue') },
+      { path: '/categorias', component: () => import('pages/CategoriesPage.vue') },
+      { path: '/sobre', component: () => import('pages/AboutPage.vue') },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ]
 
 export default routes
